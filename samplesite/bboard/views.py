e0f027fa82c92232d756550ee5b307be7d5a2e78
Carden <carden.ruby@gmail.com>
2021-04-27 22:39:02 +0300

    render templates

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,10 +1,19 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# from django.template import loader
 
+from django.shortcuts import render
 from .models import Bb
 
 
 def index(request):
-    s = 'Список объявлений\r\n\r\n\r\n'
-    for bb in Bb.objects.order_by('-published'):
-        s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-    return HttpResponse(s, content_type='text/plain; charset=utf8')
+    # # Загружаем шаблон, из модуля django.template.loader. В качестве параметра указываем строку с путем к файлу
+    # # шаблона от папки templates.
+    # template = loader.get_template('bboard/index.html')
+    # bbs = Bb.objects.order_by('-published')
+    # # Формируем контекст шаблона - набор данных, которые будут выведены на генерируемой странице.
+    # # Контекст шаблона должен представлять собой обычный словарь Python, элементы которого преобразуются в доступные
+    # # внутри шаблона переменные, одноименные ключам этих элементов.
+    # context = {'bbs': bbs}
+    bbs = Bb.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs': bbs})
+
