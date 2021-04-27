0ed7b52afe998505d0b0d9ef10edb11b8f96bbde
Carden <carden.ruby@gmail.com>
2021-04-27 21:31:48 +0300

    include urls and add model

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Bb(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
+    # Параметр db_index при присваивании ему значения True укажет создать для этого поля индекс.
+    published = models.DateTimeField(auto_now_add=True, db_index=True)
+
