061c775c0e0ad1065432a1978a5c2dd54cfde1cf
Carden <carden.ruby@gmail.com>
2021-04-28 22:52:00 +0300

    add class editor in admin.py

--- samplesite/bboard/admin.py
+++ samplesite/bboard/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 
 from .models import Bb
 
-admin.site.register(Bb)
+
+class BbAdmin(admin.ModelAdmin):
+    """ Класс редактора объявляется как производный от класса ModelAdmin из модуля
+        django.contrib.admin. Он содержит набор атрибутов класса,
+        которые задают параметры представления модели. """
+    # list_display - последовательность имен полей, которые должны выводиться в списке записей;
+    list_display = ('title', 'content', 'price', 'published')
+
+    # list_display_links - последовательность имен полей, которые должны быть преобразованы в гиперссылки,
+    # ведущие на страницу правки записи;
+    list_display_links = ('title', 'content')
+
+    # search_fields - последовательность имен полей, по которым должна выполняться фильтрация.
+    search_fields = ('title', 'content')
+
+
+admin.site.register(Bb, BbAdmin)
+
