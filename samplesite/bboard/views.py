7db702712e7a8948231d2e054ba09406c7fbc727
Carden <carden.ruby@gmail.com>
2021-04-29 20:02:11 +0300

    add method in views.py and templates - by_rubric.html

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -3,17 +3,22 @@
 
 from django.shortcuts import render
 from .models import Bb
+from .models import Rubric
 
 
 def index(request):
-    # # Загружаем шаблон, из модуля django.template.loader. В качестве параметра указываем строку с путем к файлу
-    # # шаблона от папки templates.
-    # template = loader.get_template('bboard/index.html')
-    # bbs = Bb.objects.order_by('-published')
-    # # Формируем контекст шаблона - набор данных, которые будут выведены на генерируемой странице.
-    # # Контекст шаблона должен представлять собой обычный словарь Python, элементы которого преобразуются в доступные
-    # # внутри шаблона переменные, одноименные ключам этих элементов.
-    # context = {'bbs': bbs}
-    bbs = Bb.objects.all
-    return render(request, 'bboard/index.html', {'bbs': bbs})
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bbord/by_rubric.html', context)
+
+
 
