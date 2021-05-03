9092e86d2df0807da8a39a3db36ddcac20056d79
Carden <carden.ruby@gmail.com>
2021-05-03 21:02:08 +0300

    add class BbForm and CreateView

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,9 +1,22 @@
 # from django.http import HttpResponse
 # from django.template import loader
-
+from django.views.generic.edit import CreateView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from .models import Bb
 from .models import Rubric
+from .forms import BbForm
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
 
 
 def index(request):
