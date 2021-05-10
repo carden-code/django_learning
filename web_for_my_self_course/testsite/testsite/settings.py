d7bcc6df3deebfd861e59367c85e0558f61b3a8e
Carden <carden.ruby@gmail.com>
2021-05-10 20:24:45 +0300

    fix budgs

--- web_for_my_self_course/testsite/testsite/settings.py
+++ web_for_my_self_course/testsite/testsite/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'testsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite',
     }
 }
 
