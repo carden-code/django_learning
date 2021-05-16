e06d046d50a75332c0c85729a641ac640986a1a7
Carden <carden.ruby@gmail.com>
2021-05-16 19:16:17 +0300

    edit model News - category(del null=True)

--- web_for_my_self_course/testsite/news/models.py
+++ web_for_my_self_course/testsite/news/models.py
@@ -9,7 +9,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Фото')
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey(to='Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
+    category = models.ForeignKey(to='Category', on_delete=models.PROTECT, verbose_name='Категория')
 
     def __str__(self):
         return self.title
