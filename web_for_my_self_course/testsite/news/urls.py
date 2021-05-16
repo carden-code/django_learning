8d5875621ee194af0c32e139e35b1af75e5b4692
Carden <carden.ruby@gmail.com>
2021-05-16 22:20:18 +0300

    refactoring

--- web_for_my_self_course/testsite/news/urls.py
+++ web_for_my_self_course/testsite/news/urls.py
@@ -4,7 +4,8 @@ from news.views import *
 urlpatterns = [
     path('', HomeNews.as_view(), name='home'),
     # path('', index, name='home'),
-    path('category/<int:category_id>/', get_category, name='category'),
+    path('category/<int:category_id>/', NewsByCategory.as_view(), name='category'),
+    # path('category/<int:category_id>/', get_category, name='category'),
     path('news/<int:news_id>/', view_news, name='view_news'),
     path('news/add-news/', add_news, name='add_news')
 ]
