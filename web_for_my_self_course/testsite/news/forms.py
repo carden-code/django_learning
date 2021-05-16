c2d61c4e7a8cd81b01d4e312f245246da6187535
Carden <carden.ruby@gmail.com>
2021-05-16 19:01:25 +0300

    refactoring forms.py add ModelForm

--- web_for_my_self_course/testsite/news/forms.py
+++ web_for_my_self_course/testsite/news/forms.py
@@ -1,15 +1,15 @@
 from django import forms
-from .models import Category
+from .models import News
 
 
-class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150, label='Название', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    content = forms.CharField(label='Контент', required=False, widget=forms.Textarea(attrs={'class': 'form-control',
-                                                                                            'rows': 5}))
-    is_published = forms.BooleanField(label='Опубликовано ?',
-                                      initial=True,
-                                      widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
-    category = forms.ModelChoiceField(label='Категория',
-                                      empty_label='Выберите категорию',
-                                      queryset=Category.objects.all(),
-                                      widget=forms.Select(attrs={'class': 'form-select'}))
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        # fields = '__all__'
+        fields = ['title', 'content', 'is_published', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'is_published': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'category': forms.Select(attrs={'class': 'form-select'})
+        }
