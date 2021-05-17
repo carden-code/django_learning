97e26ef1182e44b7dcf7aedf60a2f05f9be711eb
Carden <carden.ruby@gmail.com>
2021-05-17 15:07:34 +0300

    add class ViewNews(DetailView) and html

--- web_for_my_self_course/testsite/news/views.py
+++ web_for_my_self_course/testsite/news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import News, Category
 from .forms import NewsForm
@@ -54,13 +54,19 @@ def get_category(request, category_id):
     return render(request, template_name='news/category.html', context=context)
 
 
-def view_news(request, news_id):
-    # news_item = News.objects.get(pk=news_id)
-    news_item = get_object_or_404(News, pk=news_id)
-    context = {
-        'news_item': news_item,
-    }
-    return render(request, template_name='news/view_news.html', context=context)
+# def view_news(request, news_id):
+#     # news_item = News.objects.get(pk=news_id)
+#     news_item = get_object_or_404(News, pk=news_id)
+#     context = {
+#         'news_item': news_item,
+#     }
+#     return render(request, template_name='news/view_news.html', context=context)
+
+class ViewNews(DetailView):
+    model = News
+    context_object_name = 'news_item'
+    # pk_url_kwarg = 'news_id'
+    # template_name = 'news/news_detail.html'
 
 
 def add_news(request):
