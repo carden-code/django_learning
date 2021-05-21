eb4eada3dad7f1d72c6fcf9975fc0349f3531e2b
Carden <carden.ruby@gmail.com>
2021-05-21 20:59:18 +0300

    del comment in urls

--- web_for_my_self_course/testsite/news/urls.py
+++ web_for_my_self_course/testsite/news/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('category/<int:category_id>/', NewsByCategory.as_view(), name='category'),
     # path('category/<int:category_id>/', get_category, name='category'),
     path('news/<int:pk>/', ViewNews.as_view(), name='view_news'),
-    # path('news/add-news/', add_news, name='add_news')
     path('news/add-news/', CreateNews.as_view(), name='add_news')
 ]
