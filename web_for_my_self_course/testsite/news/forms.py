9fa3957c712a0c32c6ec4391b247c7d82a364613
Carden <carden.ruby@gmail.com>
2021-06-03 12:31:22 +0300

    add help_text in form Register

--- web_for_my_self_course/testsite/news/forms.py
+++ web_for_my_self_course/testsite/news/forms.py
@@ -7,7 +7,10 @@ from django.contrib.auth.models import User
 
 
 class UserRegisterForm(UserCreationForm):
-    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(label='Имя пользователя',
+                               help_text='Максимум 150 символов.',
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
     password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     password2 = forms.CharField(label='Подтверждение пароля',
