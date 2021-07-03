f5512c7632c68b7244932d13225a4de41fe998e9
Carden <carden.ruby@gmail.com>
2021-07-03 22:18:12 +0300

    requirements.txt

--- web_for_my_self_course/dartblog/blog/admin.py
+++ web_for_my_self_course/dartblog/blog/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from .models import *
+
+
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Post
+        fields = '__all__'
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -9,6 +18,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    form = PostAdminForm
 
 
 class TagAdmin(admin.ModelAdmin):
