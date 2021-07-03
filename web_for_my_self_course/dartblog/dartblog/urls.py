151b4466878b467dd4078cfbf330bb9aa0a8c803
Carden <carden.ruby@gmail.com>
2021-07-03 21:51:51 +0300

    add django_debug_toolbar

--- web_for_my_self_course/dartblog/dartblog/urls.py
+++ web_for_my_self_course/dartblog/dartblog/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
+
 ]
 
 if settings.DEBUG:
