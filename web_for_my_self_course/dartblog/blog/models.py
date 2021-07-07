563ad9afe806021ca6cd49d8ea040848cc333e70
Carden <carden.ruby@gmail.com>
2021-07-07 20:50:29 +0300

    add list_display in PostAdmin

--- web_for_my_self_course/dartblog/blog/models.py
+++ web_for_my_self_course/dartblog/blog/models.py
@@ -23,6 +23,8 @@ class Category(models.Model):
         return self.title
 
     class Meta:
+        verbose_name = 'Категория(ю)'
+        verbose_name_plural = 'Категории'
         ordering = ['title']
 
 
@@ -34,22 +36,26 @@ class Tag(models.Model):
         return self.title
 
     class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
         ordering = ['title']
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name='Название')
     slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
     author = models.CharField(max_length=100)
-    content = models.TextField(blank=True)
+    content = models.TextField(blank=True, verbose_name='Контент')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликована')
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Фото')
     views = models.IntegerField(default=0, verbose_name='Колличество просмотров')
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
-    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts', verbose_name='Категория')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts', verbose_name='Тег')
 
     def __str__(self):
         return self.title
 
     class Meta:
+        verbose_name = 'Статья(ю)'
+        verbose_name_plural = 'Статьи'
         ordering = ['-created_at']
